24d89f6c6caa0fa67348d8551b67ed83fdd34979
Thatipalli Santhoshini <5229765santhoshini9th@gmail.com>
2025-07-24 17:30:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,70 @@
-//api not working
-const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies"
+const BASE_URL = "https://api.frankfurter.app/latest";
 
-const dropdowns=document.querySelectorAll(".dropdown select");
-const btn=document.querySelector("form button");
-const fromCurr= document.querySelector(".from select");
-const toCurr= document.querySelector(".to select");
-const msg=document.querySelector(".msg");
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
-for(let select of dropdowns) {
-    for(currCode in countryList){
-        let newOption=document.createElement("option");
-        newOption.innerText=currCode;
-        newOption.value=currCode;//this function is basically adding all countries currencies
-        select.append(newOption);
-        if(select.name === "from" && currCode === "USD") {
-            newOption.selected="selected";
-        }
-        else if (select.name === "to" && currCode === "INR") {
-            newOption.selected ="selected";
-        }
-        select.append(newOption);
-    }
-    select.addEventListener("change",(evt)=>{
-    updateFlag(evt.target);// evt is event object and evt.target is place where change took place
-    });
+// Populate currency dropdowns
+for (let select of dropdowns) {
+  for (let code in countryList) {
+    const option = document.createElement("option");
+    option.value = code;
+    option.textContent = code;
+
+    if (select.name === "from" && code === "USD") option.selected = true;
+    if (select.name === "to" && code === "INR") option.selected = true;
+
+    select.append(option);
+  }
+
+  // Flag change on select
+  select.addEventListener("change", (e) => {
+    const code = e.target.value;
+    const countryCode = countryList[code];
+    const img = e.target.parentElement.querySelector("img");
+    img.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  });
 }
 
+// Convert currency using API
+async function updateExchangeRate() {
+  let amountInput = document.querySelector(".amount input");
+  let amtVal = parseFloat(amountInput.value);
 
- const updateFlag = (element) => {//in element we have select
-    let currCode=element.value;
-    let countryCode= countryList[currCode];//IN 
-    let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
-   let img= element.parentElement.querySelector("img");//here both for from and to get changed like anything we change image will be correct coz in select both from and to comes
-   img.src=newSrc;
- };
- 
- 
- const updateExchangeRate = async () =>{
-    let amount = document.querySelector(".amount input");
-    let amtVal=amount.value;
-    if(amtVal===""|| amtVal < 1){
-        amtVal=1;
-        amount.value="1";
-    }
+  if (isNaN(amtVal) || amtVal <= 0) {
+    amtVal = 1;
+    amountInput.value = "1";
+  }
 
-    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-    let response=await fetch(URL);
-    let data= await response.json();
-    let rate=data[toCurr.value.toLowerCase()];
-    let finalAmount = amtVal*rate;
-    msg.innerText=`${amtVal} ${fromCurr.value}=${finalAmount} ${toCurr.value}`;
- }
- btn.addEventListener("click",  (evt) => {
-     evt.preventDefault();//generally when we submit form it reloads and some default changes happen ,u can prevent them
-    updateExchangeRate();
+  const from = fromCurr.value;
+  const to = toCurr.value;
 
- });
-window.addEventListener("load",()=>{
- updateExchangeRate();
-})
+  if (from === to) {
+    msg.innerText = `${amtVal} ${from} = ${amtVal} ${to}`;
+    return;
+  }
+
+  const URL = `${BASE_URL}?amount=${amtVal}&from=${from}&to=${to}`;
+
+  try {
+    const res = await fetch(URL);
+    const data = await res.json();
+    console.log(data);
+    const rate = data.rates[to];
+
+    msg.innerText = `${amtVal} ${from} = ${rate.toFixed(2)} ${to}`;
+  } catch (err) {
+    msg.innerText = "Conversion failed. Try again.";
+    console.error("Error fetching rate:", err);
+  }
+}
+
+// Event listeners
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", updateExchangeRate);
